8aada4bfe3b28a3bf203b81427bf2de85909fe28
Gabssil <126586812+Gabssil@users.noreply.github.com>
2023-03-07 12:14:39 -0300

    Update index.php

--- projeto_web_poo/index.php
+++ projeto_web_poo/index.php
@@ -6,19 +6,19 @@
      <form method="POST" action="teste.class.php">
          nome: <input type ="text" name="nome" id="nome"><br><br>
         
-         
+         //caixa de entrada endereço
          endereco: <input type ="text" name="endereco" id="endereco"><br><br>
      
-
+         //caixa de entrada bairro
          bairro: <input type ="text" name="bairro" id="bairro"><br><br>
         
-
+         //caixa de entrada cep
          cep: <input type ="text" name="cep" id="cep"><br><br>
          
-
+         //caixa de entrada cidade
          cidade: <input type ="text" name="cidade" id="cidade"><br><br>
          
-
+         //caixa de entrada estado
          estado: <input type ="text" name="estado" id="estado"><br><br>
          <input type="submit" name="Cadastrar" value="Cadastrar">
 
